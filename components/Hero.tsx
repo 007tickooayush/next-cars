7cb4782df6a1966b45601505ffd84c87d2a5bd1d
Ayush Tickoo <ayush.tickoo@ONPASSIVE.ORG>
2023-09-04 15:52:16 +0530

    basic home page layout done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,37 @@
+"use client";
 import React from 'react'
+import CustomButton from './CustomButton';
 
-// import Image from '/next/image';
+import Image from 'next/image';
 
 const Hero = () => {
-  return (
-    <div className='hero'>Hero</div>
-  )
+
+    const handleScroll = () => {
+
+    }
+
+    return (
+        <div className='hero'>
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className='hero__title'>
+                    Find, Book or Rent a Car — Easily!
+                </h1>
+                <p className='hero__subtitle'>Streamline Your Car  rental experience with our effortless process.</p>
+
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                </div>
+                <div className="hero__image-overlay" />
+            </div>
+        </div>
+    )
 }
 
 export default Hero;
